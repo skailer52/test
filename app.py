b2e0d072e88a9c5380ca4463bb2b67cea9e4efbd
skailer52 <eduardo.olivares@skael.com>
2022-05-23 14:57:37 -0700

    more stuff

---
+++ app.py
@@ -0,0 +1,4 @@
+from test import coingecko_status
+
+
+print(coingecko_status())
--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,1 @@
 from .dummy_api_call import coingecko_status
-from .spanish import simple
-
-
-MY_CONST = 10000
-
-print("Hello world")
